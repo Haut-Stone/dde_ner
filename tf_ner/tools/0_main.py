ae9d0ae8a3c3fc9ee181f5a26514771979b391a2
石佳欢 <haut.1604.Stone@gmail.com>
2021-11-29 12:56:13 +0800

    更新了数据

--- tf_ner/tools/0_main.py
+++ tf_ner/tools/0_main.py
@@ -17,12 +17,12 @@ if __name__ == '__main__':
     os.remove('./check_data/all_rel.csv')
 
     # 对与需要生成的项目列表，生成数据
-    projects = [6, 7, 8,
-                32, 30, 34, 31,
-                9, 17, 35, 36, 24, 37, 23, 13, 16, 14,
-                20, 25, 21, 22, 18, 19, 38, 39, 40, 12,
-                28, 26, 11, 48, 33, 49, 29, 51, 15, 47,
-                27, 45, 41, 46, 44, 42, 43]
+    # projects = [6, 7, 8,
+    #             32, 30, 34, 31,
+    #             9, 17, 35, 36, 24, 37, 23, 13, 16, 14,
+    #             20, 25, 21, 22, 18, 19, 38, 39, 40, 12,
+    #             28, 26, 11, 48, 33, 49, 29, 51, 15, 47,
+    #             27, 45, 41, 46, 44, 42, 43]
     # projects = [32, 30, 34, 31]
     projects = [6, 7,
                 32, 30, 34, 31,
@@ -32,7 +32,7 @@ if __name__ == '__main__':
                 41, 42, 43]
     for project in projects:
         print(project)
-        a = DataGenerator('C:\\Users\\sjh\\Desktop\\doccano\\doccano21@11@25.db')
+        a = DataGenerator('C:\\Users\\sjh\\Desktop\\doccano\\doccano21@11@29.db')
         a.run(project)
 
     # 生成过后将所有的数据进行数据集划分
